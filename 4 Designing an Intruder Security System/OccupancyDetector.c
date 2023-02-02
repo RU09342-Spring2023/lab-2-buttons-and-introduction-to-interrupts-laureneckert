bf77e1272d71b74cc6d708a5e524184dcde9c7a9
Lauren Eckert <laureneckert5@gmail.com>
2023-02-02 17:48:13 -0500

    added provided starting code from read me

--- 4 Designing an Intruder Security System/OccupancyDetector.c
+++ 4 Designing an Intruder Security System/OccupancyDetector.c
@@ -7,4 +7,29 @@
 
 
 
+#define ARMED_STATE 0
+#define WARNING_STATE 1
+#define ALERT_STATE 2
+
+// Put some initialization here
+
+char state = ARMED_STATE;
+
+while(1)
+{
+  switch (state) {
+    case ARMED_STATE:
+    {
+      // Do something in the ARMED state
+      // If something happens, you can move into the WARNING_STATE
+      // state = WARNING_STATE
+    }
+    case WARNING_STATE:
+    {
+      // Do something in the WARNING_STATE
+    }
+  }
+}
+
+
 
